301f8e5e9dcf64676a25df13578ab1c17fb4ce19
Len04gau <lengaualbertntje@gmail.com>
2025-09-28 19:22:44 +0100

    Delete LENGAU directory

--- LENGAU/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.lengau {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens com.example.lengau to javafx.fxml;
-    exports com.example.lengau;
-}
